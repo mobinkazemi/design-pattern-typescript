2a8e5a871bfd1bd17aa18aa3cf0aa5a3ffecbcaa
Mobin Kazemi <mobinkazemi@MacBook-Pro.local>
2023-06-26 22:27:30 +0330

    creational - singleton - parametric singleton pattern added

--- src/creational/singleton/index.ts
+++ src/creational/singleton/index.ts
@@ -1,11 +1,28 @@
 class Singleton {
     //store and cache the instance by making it static
-    private static instance:Singleton;
+    private static instance: Singleton;
     //make sure no instance is manually created
-    private constructor(){}
+    private constructor() { }
     //get instance
-    static getInstance(){
-        if(!Singleton.instance) Singleton.instance = new Singleton();
+    static getInstance() {
+        if (!Singleton.instance) Singleton.instance = new Singleton();
         return Singleton.instance;
+    }
+}
+
+
+class ParametricSingleton {
+    private param: string;
+    private static instances: Map<string, ParametricSingleton>
+    private constructor(param: string) {
+        this.param = param
+    }
+
+    static getInstance(param: string) {
+        if (!ParametricSingleton.instances.has(param)) {
+            ParametricSingleton.instances.set(param,
+                new ParametricSingleton(param))
+        }
+        return ParametricSingleton.instances.get(param);
     }
 }
--- src/creational/singleton/readme.md
+++ src/creational/singleton/readme.md
@@ -1,3 +1,6 @@
 # Singleton
 It says that only one presence of an Object should be inside a program. This approach mostly desired as making that Object ***global***.
-For example when we try to use database in a program, we can not create an object each time that gives us access to database.
+For example when we try to use database in a program, we can not create an Object each time that gives us access to database.
+
+## Disadvantages
+1. As it is obvious, this pattern avoids to send parameters in constructor as you first create that Object and so having different variants of an Object. One solution is to use a pattern called ***Parametric Singleton pattern***. In this approach we can use ***keys*** to get a desired instance from `getInstance` method
